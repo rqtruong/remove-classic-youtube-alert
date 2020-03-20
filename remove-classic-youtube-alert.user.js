78e4bd927136d72898a104a96239e1f9073b7487
rqtruong <rqtruong@uci.edu>
2020-03-19 20:33:17 -0700

    Add mutation observer to remove any further alerts created after page load

--- remove-classic-youtube-alert.user.js
+++ remove-classic-youtube-alert.user.js
@@ -11,6 +11,18 @@
 (function() {
     'use strict';
 
-    // Your code here...
     document.getElementById("old-browser-alert").remove();
+
+    // listen for creation of more alerts and remove those as well
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            for (var i = 0; i < mutation.addedNodes.length; i++){
+                console.log(mutation.addedNodes[i].id);
+                if(mutation.addedNodes[i].id === "old-browser-alert"){
+                    document.getElementById("old-browser-alert").remove();
+                }
+            }
+        })
+    });
+    observer.observe(document.getElementById("page"), { childList: true, subtree: true });
 })();
